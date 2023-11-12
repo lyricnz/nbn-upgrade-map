3ff8a087e6d42a5a6bdfdac0d1f85dad708d5769
Luke Prior <22492406+Uskompuf@users.noreply.github.com>
2023-11-12 20:59:44 +1100

    service worker update

--- site/serviceworker.js
+++ site/serviceworker.js
@@ -5,19 +5,14 @@ self.addEventListener("fetch", async (event) => {
     // Open the cache
     event.respondWith(caches.open(cacheName).then((cache) => {
         if (event.request.destination === 'image') {
-            console.log('image');
-            // Respond with the image from the cache or from the network
             return cache.match(event.request).then((cachedResponse) => {
-                console.log(cachedResponse);
-                return cachedResponse || fetch(event.request.url).then((fetchedResponse) => {
-                    // Add the network response to the cache for future visits.
-                    // Note: we need to make a copy of the response to save it in
-                    // the cache and use the original as the request response.
-                    cache.put(event.request, fetchedResponse.clone());
+                const fetchedResponse = fetch(event.request).then((networkResponse) => {
+                    cache.put(event.request, networkResponse.clone());
 
-                    // Return the network response
-                    return fetchedResponse;
+                    return networkResponse;
                 });
+
+                return cachedResponse || fetchedResponse;
             });
         } else {
             return fetch(event.request);
