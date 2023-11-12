988ac5c7e6af57a79908ff75e5129a6ab9f627d1
Luke Prior <22492406+Uskompuf@users.noreply.github.com>
2023-11-12 21:09:08 +1100

    test

--- site/serviceworker.js
+++ site/serviceworker.js
@@ -2,9 +2,8 @@ const cacheName = 'cache';
 const cacheDuration = 30; // days
 
 self.addEventListener("fetch", async (event) => {
-    // Open the cache
-    event.respondWith(caches.open(cacheName).then((cache) => {
-        if (event.request.destination === 'image') {
+    if (event.request.destination === 'image') {
+        event.respondWith(caches.open(cacheName).then((cache) => {
             return cache.match(event.request).then((cachedResponse) => {
                 const fetchedResponse = fetch(event.request).then((networkResponse) => {
                     cache.put(event.request, networkResponse.clone());
@@ -14,8 +13,8 @@ self.addEventListener("fetch", async (event) => {
 
                 return cachedResponse || fetchedResponse;
             });
-        } else {
-            return fetch(event.request);
-        }
-    }));
+        }));
+    } else {
+        return fetch(event.request);
+    }
 });
