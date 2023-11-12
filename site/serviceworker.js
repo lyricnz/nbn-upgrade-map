7dffdf42687a0d3d6a24cb159caa0cc1c67be08b
Luke Prior <22492406+Uskompuf@users.noreply.github.com>
2023-11-12 23:14:35 +1100

    fix js detection

--- site/serviceworker.js
+++ site/serviceworker.js
@@ -43,7 +43,7 @@ self.addEventListener("fetch", async (event) => {
                 });
             });
         }));
-    } else if (event.request.url.includes('/static/') || event.request.headers.get('accept').includes('text/css') || event.request.url.includes('.js') || /^https:\/\/raw\.githubusercontent.com\/LukePrior\/nbn-upgrade-map\/[^|/]{40,}\/results\/.*\.geojson/.test(event.request.url)) {
+    } else if (event.request.url.includes('/static/') || event.request.headers.get('accept').includes('text/css') || event.request.url.endsWith('.js') || /^https:\/\/raw\.githubusercontent.com\/LukePrior\/nbn-upgrade-map\/[^|/]{40,}\/results\/.*\.geojson/.test(event.request.url)) {
         event.respondWith(caches.open(`${cacheName}-persistent`).then((cache) => {
             return cache.match(event.request).then((cachedResponse) => {
                 const fetchedResponse = fetch(event.request).then((networkResponse) => {
