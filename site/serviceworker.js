fc016dd3ff97e0fe5c97463430c83f705aa1476d
Luke Prior <22492406+Uskompuf@users.noreply.github.com>
2023-11-12 21:55:19 +1100

    map test

--- site/serviceworker.js
+++ site/serviceworker.js
@@ -5,7 +5,13 @@ self.addEventListener("fetch", async (event) => {
     if (event.request.destination === 'image') {
         event.respondWith(caches.open(cacheName).then((cache) => {
             return cache.match(event.request).then((cachedResponse) => {
-                const fetchedResponse = fetch(event.request).then((networkResponse) => {
+                if (isValid(cachedResponse)) {
+                    console.log('Serving from the cache: ', event.request.url);
+                    return cachedResponse;
+                }
+
+                return fetch(event.request).then((networkResponse) => {
+                    console.log('Serving from the network: ', event.request.url);
                     let cacheCopy = networkResponse.clone();
                     let headers = new Headers(cacheCopy.headers);
                     headers.append('sw-fetched-on', new Date().getTime());
@@ -16,12 +22,30 @@ self.addEventListener("fetch", async (event) => {
                     }));
 
                     return networkResponse;
+                }).catch(() => {
+                    return cachedResponse;
                 });
-
-                return cachedResponse || fetchedResponse;
             });
         }));
     } else {
         event.respondWith(fetch(event.request));
     }
 });
+
+function isValid (response, days=cacheDuration) {
+    if (!response) {
+        return false;
+    }
+
+    let fetchedOn = response.headers.get('sw-fetched-on');
+    if (!fetchedOn) {
+        return false;
+    }
+
+    fetchedOn = parseInt(fetchedOn);
+    if (isNaN(fetchedOn)) {
+        return false;
+    }
+
+    return (new Date().getTime() - fetchedOn) < (days * 24 * 60 * 60 * 1000);
+}
