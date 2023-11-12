428b74832219abdb92b580acb7e4598ab5fecd59
Luke Prior <22492406+Uskompuf@users.noreply.github.com>
2023-11-12 20:53:29 +1100

    update service worker

--- site/serviceworker.js
+++ site/serviceworker.js
@@ -1,35 +1,24 @@
 const cacheName = 'cache';
 const cacheDuration = 30; // days
 
-self.addEventListener("fetch", event => {
-    // All requests fall into one of these categories.
-    // 1. Return if in cache and less than 30 days old or if network request fails.
-    // 2. Return from cache and update cache in the background.
-    // 3. Return if in cache else make network request and cache the result.
+self.addEventListener("fetch", async (event) => {
+    // Open the cache
+    event.respondWith(caches.open(cacheName).then((cache) => {
+        if (event.request.destination === 'image') {
+            // Respond with the image from the cache or from the network
+            return cache.match(event.request).then((cachedResponse) => {
+                return cachedResponse || fetch(event.request.url).then((fetchedResponse) => {
+                    // Add the network response to the cache for future visits.
+                    // Note: we need to make a copy of the response to save it in
+                    // the cache and use the original as the request response.
+                    cache.put(event.request, fetchedResponse.clone());
 
-    // map tiles
-    if (event.request.destination === 'image') {
-        event.respondWith(
-            caches.open(`${cacheName}-tiles`).then(cache => {
-                return cache.match(event.request).then(response => {
-                    if (isValid(response, cacheDuration)) {
-                        return response;
-                    }
-
-                    return fetch(event.request).then(response => {
-                        var copy = response.clone();
-                        copy.headers.append('sw-cache-date', new Date().toString());
-                        cache.put(event.request, copy);
-                        return response;
-                    }).catch(() => {
-                        return cache.match(event.request);
-                    });
+                    // Return the network response
+                    return fetchedResponse;
                 });
-            })
-        );
-    }
+            });
+        } else {
+            return fetch(event.request);
+        }
+    }));
 });
-
-function isValid(response, days = 30) {
-    return response && (Date.now() - new Date(response.headers.get('sw-cache-date'))) / (1000 * 60 * 60 * 24) < days;
-}
