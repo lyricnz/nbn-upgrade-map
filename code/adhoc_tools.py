9c2d373a389175ae0e30de70092db5eb76722333
lyricnz <lyricnz@users.noreply.github.com>
2024-07-05 08:37:28 +0000

    [MegaLinter] Apply linters fixes

--- code/adhoc_tools.py
+++ code/adhoc_tools.py
@@ -403,25 +403,26 @@ def generate_local_website():
     """Generate a version of the website with all data local."""
     # copy index.html -> index-local.html
     with open("./site/index.html") as f:
-        index_html = f.read().replace('main.js', 'main-local.js')
+        index_html = f.read().replace("main.js", "main-local.js")
         with open("./site/index-local.html", "w") as f:
             f.write(index_html)
 
     # copy main.js -> main-local.js
     with open("./site/main.js") as f:
-        gh_prefix = 'https://raw.githubusercontent.com/LukePrior/nbn-upgrade-map'
-        main_js = (f.read()
-                   # use local results files
-                   .replace(gh_prefix + '/main/results', '../results')
-                   .replace(gh_prefix + '/" + commit + "/results', '../results')
-                   # disable serviceworkerr
-                   .replace('navigator.serviceWorker.', '// ')
-                   # disable date selector
-                   .replace("addControlWithHTML('date-selector'", '// ')
-                   .replace('fetch(commits_url)', 'new Promise( () => {} )')
-                   # disable gtagr
-                   .replace('gtag(', '// gtag(')
-                   )
+        gh_prefix = "https://raw.githubusercontent.com/LukePrior/nbn-upgrade-map"
+        main_js = (
+            f.read()
+            # use local results files
+            .replace(gh_prefix + "/main/results", "../results")
+            .replace(gh_prefix + '/" + commit + "/results', "../results")
+            # disable serviceworkerr
+            .replace("navigator.serviceWorker.", "// ")
+            # disable date selector
+            .replace("addControlWithHTML('date-selector'", "// ")
+            .replace("fetch(commits_url)", "new Promise( () => {} )")
+            # disable gtagr
+            .replace("gtag(", "// gtag(")
+        )
 
         with open("./site/main-local.js", "w") as f:
             f.write(main_js)
